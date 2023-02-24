7529cf9c3150086b36e86c2dc12955e9ce3a2b3a
Hjalte Mann <hjalte@me.com>
2023-02-24 14:58:53 +0100

    Additions to flask webapp

--- Trap/main.py
+++ Trap/main.py
@@ -4,38 +4,90 @@
 # import screen
 
 import time
+import sqlite3
+from datetime import datetime
 
 # Webapp
 import threading
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
-
-
+import random as rand
+import pandas as pd
 
 ######## WAKE UP ########
-#light.lightPowerUp()
 
-# Check internet connection
-#helpers.checkInternet()
+def runGoodmorning():
+    #light.lightPowerUp()
 
-# Set welcome screen
-#screen.welcomeScreen()
-# Check abiotic sensor
+    # Check internet connection
+    #helpers.checkInternet()
+
+    # Set welcome screen
+    #screen.welcomeScreen()
+    # Check abiotic sensor
+
+    # Initialize BME280 sensor
+    #i2c = busio.I2C(board.SCL, board.SDA)
+    #bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
+
+    # Initialize SQLite database connection and create table to store sensor data
+    connSensor = sqlite3.connect('envVariables.db')
+    cSensor = connSensor.cursor()
+    cSensor.execute('''CREATE TABLE IF NOT EXISTS envVariables
+                (timestamp TEXT, temperature REAL, pressure REAL, humidity REAL)''')
+    
+    # Initialize SQLite database connection and create table to store sensor data
+    connVision = sqlite3.connect('vision.db')
+    cVision = connSensor.cursor()
+    cVision.execute('''CREATE TABLE IF NOT EXISTS vision
+                (timestamp TEXT, imagePath TEXT, predictions TEXT)''')
+    
+    return connSensor, cSensor, connVision, cVision
+
+
 
 
 ######## AWAKE ########
 
 def runAwake():
+
+    connSensor, cSensor, connVision, cVision = runGoodmorning()
+
     i = 0
     while True:
         print("Still running main worker: ", i)
         i += 1
+        time.sleep(2)
+
+
+        ### Environmental variables ###
+        temperature = rand.randint(12,25) #bme280.temperature
+        pressure = rand.randint(1020, 1200) #bme280.pressure
+        humidity =  rand.randint(40,80) #bme280.humidity
+
+        # Get current timestamp
+        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+        # Insert data into env database
+        cSensor.execute("INSERT INTO envVariables VALUES (?, ?, ?, ?)", (timestamp, temperature, pressure, humidity))
+        connSensor.commit()
+        print("Env. variables saved")
+
+        ### Vision ###
+
+        # Save image
+        
+        # Run inference
+
+        # Save vision info to vision databases
+
+        # Wait for 5 seconds before reading sensor data again
         time.sleep(5)
 
 
-#     updateScreen(t, cpu)
 
-#     time.sleep(1800)
+        ### Screen ###
+#       updateScreen(t, cpu)
 
 
 
@@ -54,23 +106,35 @@ def runAwake():
 
 
 ######## WEBAPP ########
-
-
-
 app = Flask(__name__)
 
 def runApp():
     app.run(debug=True, use_reloader=False, port=5000, host='0.0.0.0')
 
-
 @app.route('/')
-def hello():
-    return """
-    <h1>TECOLOGY<h1>
-    <h2>Insect Dashboard<h2>
-    """
+def index():
+    # connect to the sqlite database
+    conn = sqlite3.connect('envVariables.db')
+    c = conn.cursor()
 
+    # retrieve the latest weather data from the database
+    c.execute('SELECT temperature, humidity, pressure, timestamp FROM envVariables ORDER BY timestamp DESC LIMIT 1')
+    latest_data = c.fetchone()
+    temperature = latest_data[0]
+    humidity = latest_data[1]
+    pressure = latest_data[2]
+    timestamp = latest_data[3]
 
+    # retrieve the data for the graph
+    c.execute('SELECT temperature, humidity, pressure, timestamp FROM envVariables WHERE date(timestamp) = date("now")')
+    graph_data = c.fetchall()
+
+    # close the database connection
+    conn.close()
+
+    # render the template with the data
+    print("Variables: ", temperature, humidity, pressure)
+    return render_template('index.html', temperature=temperature, humidity=humidity, pressure=pressure, timestamp=timestamp, graph_data=graph_data)
 
 
 ### RUN PROGRAM ###
