49b172e2a886e788694c9a750b38b3eb8bec111f
Hjalte Mann <hjalte@tecology.ai>
2023-04-11 22:18:50 +0200

    Started code for production app

---
+++ Trap/app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template
+from flask_sock import Sock
+import sqlite3
+import random
+import time
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+sock = Sock(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html'), 'index.js'
+
+
+@sock.route('/echo')
+def echo(sock):
+    conn = sqlite3.connect('data/environment.db')
+    c = conn.cursor()
+    while True:
+
+        ### Environment
+        # Query the latest number from the database
+        #cursor = conn.execute("SELECT number FROM environment ORDER BY id DESC LIMIT 1")
+        x = conn.execute("SELECT temperature, pressure, humidity FROM environment ORDER BY id DESC LIMIT 1")
+        print("Hello")
+        print(x.fetchall())
+
+        # latestTemp = cursor.fetchone()[0]
+        # latestHumi = cursor.fetchone()[0]
+        # latestPress = cursor.fetchone()[0]
+
+        sock.send(str(latestTemp)) # Convert to string and send to client
+        sock.send(str(latestHumi))
+        sock.send(str(latestPress))
+
+        ### Vision
+
+
+        time.sleep(5)
+
+# @sock.route('/echo')
+# def echo(sock):
+#     while True:
+#         data = sock.receive()
+#         sock.send(data)
+#         sock.send("Helo")
+
+# Run local production with:
+# gunicorn -b 0.0.0.0:5000 --workers 4 --threads 100 sockApp:app
