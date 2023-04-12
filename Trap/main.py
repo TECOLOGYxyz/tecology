a8ce8b368282785d82a308e7b7e58dc73d0ab1b1
Hjalte Mann <hjalte@tecology.ai>
2023-04-12 13:11:52 +0200

    Worked on logic for main program

--- Trap/main.py
+++ Trap/main.py
@@ -1,134 +1,124 @@
-# from screen import updateScreen
-import light
-import helpers
-# import screen
 
+### Gloabal packages
 from configparser import ConfigParser
 import time
 import sqlite3
 from datetime import datetime
+import random as rand
+from picamera2 import Picamera2, Preview
+import os
+import logging
+import cv2
 
 # Webapp
 import threading
 from flask import Flask, render_template, request, jsonify
 
-import random as rand
-#import pandas as pd
-
-from picamera2 import Picamera2, Preview
-import time
-
-#AI
+### Local modules
+# from screen import updateScreen
+import light
+import helpers
+import screen
 import ai
 
 # Set up configs
-
+logging.basicConfig(level=logging.DEBUG)
 
 
 # Initialize SQLite database connection and create table to store sensor data
+logging.info("Setting up environment database")
 connSensor = sqlite3.connect('data/environment.db')
 cSensor = connSensor.cursor()
 cSensor.execute('''CREATE TABLE IF NOT EXISTS environment
             (id INTEGER PRIMARY KEY AUTOINCREMENT, timestamp TEXT, temperature REAL, pressure REAL, humidity REAL)''')
 
 
-### Environmental variables ###
-temperature = rand.randint(12,25) #bme280.temperature
-pressure = rand.randint(1020, 1200) #bme280.pressure
-humidity =  rand.randint(40,80) #bme280.humidity
-
-# Get current timestamp
-timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
-# Insert data into env database
-
-connSensor = sqlite3.connect('data/environment.db')
-cSensor = connSensor.cursor()
-#cSensor.execute("INSERT INTO environment VALUES (?, ?, ?, ?)", (timestamp, temperature, pressure, humidity))
-cSensor.execute('INSERT INTO environment (temperature, humidity, timestamp, pressure) VALUES (?, ?, ?, ?)', (10,20,30,40))
-connSensor.commit()
-print("Env. variables saved")
-
-
-
-#### debug up
-
-
-
-# # Initialize SQLite database connection and create table to store image data
-# connVision = sqlite3.connect('data/vision.db')
-# cVision = connSensor.cursor()
-# cVision.execute('''CREATE TABLE IF NOT EXISTS vision
-#             (timestamp TEXT, imagePath TEXT, predictions TEXT)''')
-
-# #return connSensor, cSensor, connVision, cVision
+# Initialize SQLite database connection and create table to store image data
+logging.info("Setting up vision database")
+connVision = sqlite3.connect('data/vision.db')
+cVision = connSensor.cursor()
+cVision.execute('''CREATE TABLE IF NOT EXISTS vision
+            (id INTEGER PRIMARY KEY AUTOINCREMENT, timestamp TEXT, imagePath TEXT, drawPath TEXT, predictions TEXT)''')
 
 # # Check internet connection
-# helpers.checkInternet()
+logging.info("Checking internet connection")
+helpers.checkInternet()
+
+# Set welcome screen
+#screen.welcomeScreen()
 
 
-# def runGoodmorning():
-#     light.lightPowerUp()
+def runGoodmorning():
+    light.lightPowerUp()
 
+    # Create folders for todays data
+    today = datetime.now().strftime('%Y-%m-%d')
+    dataToday = os.path.join("data", today)
 
-#     # Create folders for todays data
-
-
+    if not os.path.exists(dataToday):
+        os.makedirs(dataToday)
 
     
+    # Set up camera
+    logging.info("Setting up camera")
+    picam2 = Picamera2()
+    camera_config = picam2.create_still_configuration(main={"size": (4608, 2592)})
+    picam2.configure(camera_config)
+    picam2.start()
 
-#     # Set up camera
-#     picam2 = Picamera2()
-#     camera_config = picam2.create_still_configuration(main={"size": (4608, 2592)})
-#     picam2.configure(camera_config)
-#     picam2.start()
+    job = picam2.autofocus_cycle(wait=False)
+    success = picam2.wait(job)
 
-#     job = picam2.autofocus_cycle(wait=False)
-#     success = picam2.wait(job)
+    # Check internet connection
+    helpers.checkInternet()
 
-#     # Check internet connection
-#     helpers.checkInternet()
+    return dataToday, picam2
 
-#     # Set welcome screen
-#     screen.welcomeScreen()
 
 
 # ######## AWAKE ########
 
-# def runAwake():
+def runAwake(dataToday, picam2):
+    i = 0
+    while True:
+        print("Still running main worker: ", i)
+        i += 1
 
-#     connSensor, cSensor, connVision, cVision = runGoodmorning()
-
-#     i = 0
-#     while True:
-#         print("Still running main worker: ", i)
-#         i += 1
-#         time.sleep(2)
+#    connSensor, cSensor, connVision, cVision = runGoodmorning()
 
 
-#         ### Environmental variables ###
-#         temperature = rand.randint(12,25) #bme280.temperature
-#         pressure = rand.randint(1020, 1200) #bme280.pressure
-#         humidity =  rand.randint(40,80) #bme280.humidity
+        ### Environmental variables ###
+        temperature = rand.randint(12,25) #bme280.temperature
+        pressure = rand.randint(1020, 1200) #bme280.pressure
+        humidity =  rand.randint(40,80) #bme280.humidity
 
-#         # Get current timestamp
-#         timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        # Get current timestamp
+        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-#         # Insert data into env database
-#         cSensor.execute("INSERT INTO envVariables VALUES (?, ?, ?, ?)", (timestamp, temperature, pressure, humidity))
-#         connSensor.commit()
-#         print("Env. variables saved")
+        # Insert data into env database
+        connSensor = sqlite3.connect('data/environment.db')
+        cSensor = connSensor.cursor()
+        #cSensor.execute("INSERT INTO environment VALUES (?, ?, ?, ?)", (timestamp, temperature, pressure, humidity))
+        cSensor.execute('INSERT INTO environment (timestamp, temperature, humidity, pressure) VALUES (?, ?, ?, ?)', (timestamp,temperature,humidity,pressure))
+        connSensor.commit()
+        print("Env. variables saved")
+        time.sleep(5)
 
-#         ### Vision ###
 
-        # timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-        # #picam2.capture_file("images/" + timestamp + "_" + str(i) + ".jpg")
-        # light.lightGreenFlash()
+         ### Vision ###
 
-        # array = picam2.capture_array("main") 
+        timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        imagePath = os.path.join(dataToday, timestamp+".jpg")
 
-        # ai.inference(array)
+        
+        array = picam2.capture_array("main")
+        light.lightGreenFlash()
+        
+        ai.inference(array)
+
+        cv2.imwrite(imagePath, array)
+
 #         # Save image
         
 #         # Run inference
@@ -142,14 +132,35 @@ print("Env. variables saved")
 
 #         ### Screen ###
 # #       updateScreen(t, cpu)
-
-
-
+        ### Health checks
+        # Check CPU tempt to throttle down if getting too hot
 
 # ######## GET READY FOR BED ########
-# # Back up images to drive
-# # Back up data to drive
-# # Turn of light
+
+
+def runBedReady():
+    """
+    Preparation for sleep mode.
+    """
+    # # Zip images to
+    # # Back up data to drive
+    # # Turn of light
+    pass
+
+def runSleep():
+    """
+    Does nothing but await the time for starting a new day
+    """
+    pass
+
+
+def runNocturnal():
+    """
+    A night mode that captures images using the onboard ring light as a flash
+    """
+    pass
+
+
 
 
 
@@ -159,36 +170,71 @@ print("Env. variables saved")
 
 
 
-# ######## WEBAPP ########
-app = Flask(__name__)
+def runTrap():
+    dataToday, picam2 = runGoodmorning()
+    runAwake(dataToday, picam2)
 
-def runApp():
-    app.run(debug=True, use_reloader=False, port=5000, host='0.0.0.0')
+if __name__ == '__main__':
+    runTrap()
 
-@app.route('/')
-def index():
-    # connect to the sqlite database
-    conn = sqlite3.connect('data/environment.db')
-    c = conn.cursor()
 
-    # retrieve the latest weather data from the database
-    c.execute('SELECT temperature, humidity, pressure, timestamp FROM environment ORDER BY timestamp DESC LIMIT 1')
-    latest_data = c.fetchone()
-    temperature = latest_data[0]
-    humidity = latest_data[1]
-    pressure = latest_data[2]
-    timestamp = latest_data[3]
 
-    # retrieve the data for the graph
-    c.execute('SELECT temperature, humidity, pressure, timestamp FROM environment WHERE date(timestamp) = date("now")')
-    graph_data = c.fetchall()
 
-    # close the database connection
-    conn.close()
 
-    # render the template with the data
-    print("Variables: ", temperature, humidity, pressure)
-    return render_template('index.html', temperature=temperature, humidity=humidity, pressure=pressure, timestamp=timestamp, graph_data=graph_data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # ######## WEBAPP ########
+# app = Flask(__name__)
+
+# def runApp():
+#     app.run(debug=True, use_reloader=False, port=5000, host='0.0.0.0')
+
+# @app.route('/')
+# def index():
+#     # connect to the sqlite database
+#     conn = sqlite3.connect('data/environment.db')
+#     c = conn.cursor()
+
+#     # retrieve the latest weather data from the database
+#     c.execute('SELECT temperature, humidity, pressure, timestamp FROM environment ORDER BY timestamp DESC LIMIT 1')
+#     latest_data = c.fetchone()
+#     temperature = latest_data[0]
+#     humidity = latest_data[1]
+#     pressure = latest_data[2]
+#     timestamp = latest_data[3]
+
+#     # retrieve the data for the graph
+#     c.execute('SELECT temperature, humidity, pressure, timestamp FROM environment WHERE date(timestamp) = date("now")')
+#     graph_data = c.fetchall()
+
+#     # close the database connection
+#     conn.close()
+
+#     # render the template with the data
+#     print("Variables: ", temperature, humidity, pressure)
+#     return render_template('index.html', temperature=temperature, humidity=humidity, pressure=pressure, timestamp=timestamp, graph_data=graph_data)
 
 
 ### RUN PROGRAM ###
@@ -205,3 +251,6 @@ def index():
 
 
 #  gunicorn -b 0.0.0.0:5000 --workers 4 --threads 100 sockApp:app
+
+
+        # #picam2.capture_file("images/" + timestamp + "_" + str(i) + ".jpg")
