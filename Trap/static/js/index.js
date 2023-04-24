9bb0832a35085ac00a0c1a65e8d067a214cdb03b
Hjalte Mann <hjalte@tecology.ai>
2023-04-24 21:02:15 +0200

    Fixed js to receive the three env variables.

--- Trap/static/js/index.js
+++ Trap/static/js/index.js
@@ -1,10 +1,41 @@
 const log = (text, color) => {
-    document.getElementById('temperature').innerHTML = `<span style="color: ${color}">${text}</span><br>`;
-  };
-  const socket = new WebSocket('ws://' + location.host + '/echo');
-  socket.addEventListener('message', ev => {
-    log('Temperatur: ' + ev.data + "&deg;" + "C", 'black');
-  });
+  document.getElementById('log').innerHTML += `<span style="color: ${color}">${text}</span><br>`;
+};
+
+const socket = new WebSocket('ws://' + location.host + '/echo');
+socket.addEventListener('message', ev => {
+  const data = JSON.parse(ev.data);
+  document.getElementById('log').innerHTML = ''; // Clear existing content
+  log('Temperature: ' + data.temperature + '&deg;C', 'black');
+  log('Humidity: ' + data.humidity + '%', 'blue');
+  log('Pressure: ' + data.pressure + 'hPa', 'green');
+});
+
+// const log = (text, color) => {
+//   document.getElementById('log').innerHTML += `<span style="color: ${color}">${text}</span><br>`;
+// };
+
+// const socket = new WebSocket('ws://' + location.host + '/echo');
+// socket.addEventListener('message', ev => {
+//   const data = JSON.parse(ev.data);
+//   log('Temperature: ' + data.temperature + '&deg;C', 'black');
+//   log('Humidity: ' + data.humidity + '%', 'blue');
+//   log('Pressure: ' + data.pressure + 'hPa', 'green');
+// });
+
+
+
+
+
+
+
+//const log = (text, color) => {
+ //   document.getElementById('temperature').innerHTML = `<span style="color: ${color}">${text}</span><br>`;
+ // };
+ // const socket = new WebSocket('ws://' + location.host + '/echo');
+ // socket.addEventListener('message', ev => {
+  //  log('Temperatur: ' + ev.data + "&deg;" + "C", 'black');
+  //});
 
   // const log2 = (text, color) => {
   //   document.getElementById('log2').innerHTML = `<span style="color: ${color}">${text}</span><br>`;
